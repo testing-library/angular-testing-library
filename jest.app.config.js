b260887f119a4025fa9b214d261185a1a55800e4
Tim Deschryver <timdeschryver@outlook.com>
2018-06-14 16:14:53 +0200

    build: reuse test commands on test

--- jest.app.config.js
+++ jest.app.config.js
@@ -3,7 +3,6 @@ const baseConfig = require('./jest.base.config');
 module.exports = {
   ...baseConfig,
   roots: ['<rootDir>/src'],
-  modulePathIgnorePatterns: ['<rootDir>/projects'],
   moduleNameMapper: {
     'ngx-testing-library': '<rootDir>/dist/ngx-testing-library',
   },
