7c8f02a7c056c9f43ee754c4b0e511a5579ec202
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2019-06-07 23:08:51 +0200

    chore: add @testing-library migration (#23)
    This change was manually released on npm as `@angular-extensions/testing-library@5.1.2` to not intervene with the current version.

---
+++ projects/testing-library/migrations/5_1_2/rules/noAngularExtensionsImportRule.ts
@@ -0,0 +1,26 @@
+import * as ts from 'typescript';
+import { Replacement, RuleFailure, Rules } from 'tslint';
+import { tsquery } from '@phenomnomnominal/tsquery';
+
+const IMPORT_QUERY = `ImportDeclaration StringLiteral[value="@angular-extensions/testing-library"]`;
+
+const FAILURE_MESSAGE =
+  'Found the library `@angular-extensions/testing-library`, use `@testing-library/angular` instead.';
+
+export class Rule extends Rules.AbstractRule {
+  public apply(ast: ts.SourceFile): Array<RuleFailure> {
+    const imports = this.getImports(ast);
+    return imports;
+  }
+
+  private getImports(ast: ts.SourceFile): Array<RuleFailure> {
+    return tsquery(ast, IMPORT_QUERY).map(result => {
+      // replace text between (single) quotes
+      const replacement = new Replacement(result.getStart() + 1, result.getWidth() - 2, '@testing-library/angular');
+      const start = result.getStart();
+      const end = result.getEnd();
+
+      return new RuleFailure(ast, start, end, FAILURE_MESSAGE, this.ruleName, replacement);
+    });
+  }
+}
