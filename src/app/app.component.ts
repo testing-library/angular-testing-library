fd6ba7e9cc28e9b0be256c37d67b63112872459c
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2019-08-29 09:39:26 +0200

    feat: import NoopAnimationsModule by default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Store } from '@ngrx/store';
 import { GreetService } from './greet.service';
 import { FormBuilder, Validators } from '@angular/forms';
@@ -7,8 +8,31 @@ import { FormBuilder, Validators } from '@angular/forms';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style({
+          height: '200px',
+          opacity: 1,
+          backgroundColor: 'yellow',
+        }),
+      ),
+      state(
+        'closed',
+        style({
+          height: '100px',
+          opacity: 0.5,
+          backgroundColor: 'green',
+        }),
+      ),
+      transition('open => closed', [animate('1s')]),
+      transition('closed => open', [animate('0.5s')]),
+    ]),
+  ],
 })
 export class AppComponent {
+  isOpen = true;
   title = 'app';
   form = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(2)]],
@@ -21,5 +45,11 @@ export class AppComponent {
     this.greetService.greet();
   }
 
-  onSubmit() {}
+  onSubmit() {
+    console.log('Form submitted: ', this.form.value);
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ReactiveFormsModule, StoreModule.forRoot({})],
+  imports: [BrowserModule, ReactiveFormsModule, BrowserAnimationsModule, StoreModule.forRoot({})],
   providers: [],
   bootstrap: [AppComponent],
 })
