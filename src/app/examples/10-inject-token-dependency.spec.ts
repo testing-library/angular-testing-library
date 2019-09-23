4b9fbcdfa429cd2fc54514bafb7ea53b3f2cc914
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2019-09-23 18:30:44 +0200

    docs: add example to inject  a token

---
+++ src/app/examples/10-inject-token-dependency.spec.ts
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/angular';
+
+import { DataInjectedComponent, DATA } from './10-inject-token-dependency';
+
+test('injects data into the component', async () => {
+  const component = await render(DataInjectedComponent, {
+    providers: [
+      {
+        provide: DATA,
+        useValue: { text: 'Hello boys and girls'}
+      }
+    ]
+  });
+
+  expect(component.getByText(/Hello boys and girls/i)).toBeInTheDocument();
+});
---
+++ src/app/examples/10-inject-token-dependency.ts
@@ -0,0 +1,11 @@
+import { Component, InjectionToken, Inject } from '@angular/core';
+
+export const DATA = new InjectionToken<{text: string}>('Components Data');
+
+@Component({
+  selector: 'app-fixture',
+  template: `{{ data.text }}`,
+})
+export class DataInjectedComponent {
+  constructor(@Inject(DATA) public data: {text: string}){}
+}
