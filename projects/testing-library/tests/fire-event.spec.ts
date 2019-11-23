e41c034754360cc7c87afe4de9fc78ad9905745f
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2019-11-23 15:52:05 +0100

    fix: don't call detectChanges when fixture is destroyed (#57)

---
+++ projects/testing-library/tests/fire-event.spec.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { render } from '../src/public_api';
+
+@Component({
+  selector: 'fixture',
+  template: `
+    <input type="text" data-testid="input" />
+  `,
+})
+class FixtureComponent {}
+
+test('does not call detect changes when fixture is destroyed', async () => {
+  const component = await render(FixtureComponent);
+
+  component.fixture.destroy();
+
+  // should otherwise throw
+  component.input(component.getByTestId('input'), { target: { value: 'Bonjour' } });
+  component.type(component.getByTestId('input'), 'Alles klar');
+});
