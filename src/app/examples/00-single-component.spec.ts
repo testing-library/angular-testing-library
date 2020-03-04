f18574ae4263a76807e100e3dab88531e9962b5a
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2020-03-04 20:02:44 +0100

    refactor: use screen to query for elements

--- src/app/examples/00-single-component.spec.ts
+++ src/app/examples/00-single-component.spec.ts
@@ -1,20 +1,20 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
 
 import { SingleComponent } from './00-single-component';
 
 test('renders the current value and can increment and decrement', async () => {
-  const component = await render(SingleComponent);
+  const { click } = await render(SingleComponent);
 
-  const incrementControl = component.getByText('Increment');
-  const decrementControl = component.getByText('Decrement');
-  const valueControl = component.getByTestId('value');
+  const incrementControl = screen.getByText('Increment');
+  const decrementControl = screen.getByText('Decrement');
+  const valueControl = screen.getByTestId('value');
 
   expect(valueControl.textContent).toBe('0');
 
-  component.click(incrementControl);
-  component.click(incrementControl);
+  click(incrementControl);
+  click(incrementControl);
   expect(valueControl.textContent).toBe('2');
 
-  component.click(decrementControl);
+  click(decrementControl);
   expect(valueControl.textContent).toBe('1');
 });
--- src/app/examples/03-forms.spec.ts
+++ src/app/examples/03-forms.spec.ts
@@ -1,35 +1,35 @@
 import { ReactiveFormsModule } from '@angular/forms';
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
 
 import { FormsComponent } from './03-forms';
 
 test('is possible to fill in a form and verify error messages (with the help of jest-dom https://testing-library.com/docs/ecosystem-jest-dom)', async () => {
-  const component = await render(FormsComponent, {
+  const { type, blur, selectOptions } = await render(FormsComponent, {
     imports: [ReactiveFormsModule],
   });
 
-  const nameControl = component.getByLabelText('Name');
-  const scoreControl = component.getByLabelText(/score/i);
-  const colorControl = component.getByLabelText('color', { exact: false });
-  const errors = component.getByRole('alert');
+  const nameControl = screen.getByLabelText('Name');
+  const scoreControl = screen.getByLabelText(/score/i);
+  const colorControl = screen.getByLabelText('color', { exact: false });
+  const errors = screen.getByRole('alert');
 
-  expect(errors).toContainElement(component.queryByText('name is required'));
-  expect(errors).toContainElement(component.queryByText('score must be greater than 1'));
-  expect(errors).toContainElement(component.queryByText('color is required'));
+  expect(errors).toContainElement(screen.queryByText('name is required'));
+  expect(errors).toContainElement(screen.queryByText('score must be greater than 1'));
+  expect(errors).toContainElement(screen.queryByText('color is required'));
 
   expect(nameControl).toBeInvalid();
-  component.type(nameControl, 'Tim');
-  component.type(scoreControl, '12');
-  component.blur(scoreControl);
-  component.selectOptions(colorControl, 'Green');
+  type(nameControl, 'Tim');
+  type(scoreControl, '12');
+  blur(scoreControl);
+  selectOptions(colorControl, 'Green');
 
-  expect(component.queryByText('name is required')).not.toBeInTheDocument();
-  expect(component.queryByText('score must be lesser than 10')).toBeInTheDocument();
-  expect(component.queryByText('color is required')).not.toBeInTheDocument();
+  expect(screen.queryByText('name is required')).not.toBeInTheDocument();
+  expect(screen.queryByText('score must be lesser than 10')).toBeInTheDocument();
+  expect(screen.queryByText('color is required')).not.toBeInTheDocument();
 
   expect(scoreControl).toBeInvalid();
-  component.type(scoreControl, 7);
-  component.blur(scoreControl);
+  type(scoreControl, 7);
+  blur(scoreControl);
   expect(scoreControl).toBeValid();
 
   expect(errors).not.toBeInTheDocument();
@@ -38,7 +38,7 @@ test('is possible to fill in a form and verify error messages (with the help of 
   expect(scoreControl).toHaveValue(7);
   expect(colorControl).toHaveValue('G');
 
-  const form = component.getByTestId('my-form');
+  const form = screen.getByTestId('my-form');
   expect(form).toHaveFormValues({
     name: 'Tim',
     score: 7,
