9a4245fc5df6b59539f9793626ce0e93d18c8bcc
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2020-12-04 10:39:33 +0100

    docs: add async component example

---
+++ apps/example-app/app/examples/14-async-component.ts
@@ -0,0 +1,27 @@
+import { ApplicationInitStatus, Component, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { delay, filter, mapTo } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-fixture',
+  template: `
+    <button (click)="load()">Load</button>
+    <div *ngIf="data$ | async as data">{{ data }}</div>
+  `,
+})
+export class AsyncComponent implements OnDestroy {
+  actions = new Subject<string>();
+  data$ = this.actions.pipe(
+    filter((x) => x === 'LOAD'),
+    mapTo('Hello world'),
+    delay(10_000),
+  );
+
+  load() {
+    this.actions.next('LOAD');
+  }
+
+  ngOnDestroy() {
+    this.actions.complete();
+  }
+}
