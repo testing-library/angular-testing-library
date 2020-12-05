acaa8fe576d2a4dcacdb7e05fd4277fe62094c67
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2020-12-05 14:14:45 +0100

    docs: add async component example (#171)

---
+++ apps/example-app/app/examples/14-async-component.spec.ts
@@ -0,0 +1,16 @@
+import { fakeAsync, tick } from '@angular/core/testing';
+import { render, screen, fireEvent } from '@testing-library/angular';
+
+import { AsyncComponent } from './14-async-component';
+
+test('can use fakeAsync utilities', fakeAsync(async () => {
+  await render(AsyncComponent);
+
+  const load = await screen.findByRole('button', { name: /load/i });
+  fireEvent.click(load);
+
+  tick(10_000);
+
+  const hello = await screen.findByText('Hello world');
+  expect(hello).toBeInTheDocument();
+}));
