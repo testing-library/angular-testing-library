39796c4e1f1724720be3c4a56f4780c4d2a58be4
Bo Vandersteene <bo.vandersteene@reibo.be>
2020-12-09 14:19:59 +0100

    feat: create mock with values (#172)

--- projects/jest-utils/src/lib/create-mock.ts
+++ projects/jest-utils/src/lib/create-mock.ts
@@ -29,6 +29,19 @@ export function createMock<T>(type: Type<T>): Mock<T> {
   return mock;
 }
 
+export function createMockWithValues<T, K extends keyof T>(
+  type: Type<T>,
+  values: Partial<Record<K, T[K]>>,
+): Mock<T> {
+  const mock = createMock(type);
+
+  Object.entries(values).forEach(([field, value]) => {
+    (mock as any)[field] = value;
+  });
+
+  return mock;
+}
+
 export function provideMock<T>(type: Type<T>): Provider {
   return {
     provide: type,
