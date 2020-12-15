6be4511d60cbe3b73a490d6ad5a3994f223f712a
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2020-12-15 09:51:35 +0100

    docs: add dialog example (#175)

---
+++ apps/example-app/app/examples/15-dialog.component.spec.ts
@@ -0,0 +1,63 @@
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { render, screen, fireEvent, waitForElementToBeRemoved } from '@testing-library/angular';
+
+import { DialogComponent, DialogContentComponent, DialogContentComponentModule } from './15-dialog.component';
+
+test('dialog closes', async () => {
+  const closeFn = jest.fn();
+  await render(DialogContentComponent, {
+    imports: [MatDialogModule],
+    providers: [
+      {
+        provide: MatDialogRef,
+        useValue: {
+          close: closeFn,
+        },
+      },
+    ],
+  });
+
+  const cancelButton = await screen.findByRole('button', { name: /cancel/i });
+  fireEvent.click(cancelButton);
+
+  expect(closeFn).toHaveBeenCalledTimes(1);
+});
+
+test('opens and closes the dialog with buttons', async () => {
+  await render(DialogComponent, {
+    imports: [MatDialogModule, DialogContentComponentModule],
+  });
+
+  const openDialogButton = await screen.findByRole('button', { name: /open dialog/i });
+  fireEvent.click(openDialogButton);
+
+  await screen.findByRole('dialog');
+  await screen.findByRole('heading', { name: /dialog title/i });
+
+  const cancelButton = await screen.findByRole('button', { name: /cancel/i });
+  fireEvent.click(cancelButton);
+
+  await waitForElementToBeRemoved(() => screen.getByRole('dialog'));
+
+  const dialogTitle = screen.queryByRole('heading', { name: /dialog title/i });
+  expect(dialogTitle).not.toBeInTheDocument();
+});
+
+test('closes the dialog via the backdrop', async () => {
+  await render(DialogComponent, {
+    imports: [MatDialogModule, DialogContentComponentModule],
+  });
+
+  const openDialogButton = await screen.findByRole('button', { name: /open dialog/i });
+  fireEvent.click(openDialogButton);
+
+  await screen.findByRole('dialog');
+  await screen.findByRole('heading', { name: /dialog title/i });
+
+  fireEvent.click(document.querySelector('.cdk-overlay-backdrop'));
+
+  await waitForElementToBeRemoved(() => screen.getByRole('dialog'));
+
+  const dialogTitle = screen.queryByRole('heading', { name: /dialog title/i });
+  expect(dialogTitle).not.toBeInTheDocument();
+});
