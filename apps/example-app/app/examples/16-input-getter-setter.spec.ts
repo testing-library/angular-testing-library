e8894fa2bad8799d375c3377dd18b210187abc71
Gleb Irovich <gleb.irovich@locatee.ch>
2021-03-24 22:16:33 +0100

    test: add a test to cover re-rendering scenario

--- apps/example-app/app/examples/16-input-getter-setter.spec.ts
+++ apps/example-app/app/examples/16-input-getter-setter.spec.ts
@@ -9,3 +9,17 @@ test('should run logic in the input setter and getter', async () => {
   expect(valueControl.textContent).toBe('I am value from setter Angular');
   expect(getterValueControl.textContent).toBe('I am value from getter Angular');
 });
+
+test('should run logic in the input setter and getter while re-rendering', async () => {
+  const component = await render(InputGetterSetter, { componentProperties: { value: 'Angular' } });
+  const valueControl = screen.getByTestId('value');
+  const getterValueControl = screen.getByTestId('value-getter');
+
+  expect(valueControl.textContent).toBe('I am value from setter Angular');
+  expect(getterValueControl.textContent).toBe('I am value from getter Angular');
+
+  await component.rerender({ value: 'React' });
+
+  expect(valueControl.textContent).toBe('I am value from setter React');
+  expect(getterValueControl.textContent).toBe('I am value from getter React');
+});
