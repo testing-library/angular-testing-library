5a3d7739e6d30347e1867fe1aefe907fb9c3fd3a
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2021-06-16 18:04:10 +0200

    remove ci flag

--- apps/example-app-karma/karma.conf.js
+++ apps/example-app-karma/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
       logLevel: config.LOG_INFO,
       autoWatch: true,
       browsers: ['Chrome'],
-      singleRun: false,
+      singleRun: true,
       restartOnFileChange: true,
     });
   } catch (err) {
