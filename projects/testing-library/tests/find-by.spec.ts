41bb00fd88eab6d3bb9a8d29342d02fa891bcecb
arjen.althoff <althoff.a@tkppensioen.nl>
2021-07-07 08:32:18 +0200

    refactor(find-by-text): use utility functions instead of screen

--- projects/testing-library/tests/find-by.spec.ts
+++ projects/testing-library/tests/find-by.spec.ts
@@ -25,12 +25,16 @@ describe('screen', () => {
 
 describe('rendered component', () => {
   it('waits for element to be added to the DOM', async () => {
-    await render(FixtureComponent);
-    await expect(screen.findByText('I am visible')).resolves.toBeTruthy();
+    const { findByText } = await render(FixtureComponent);
+    /// We whish to test the utility function from `render` here.
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    await expect(findByText('I am visible')).resolves.toBeTruthy();
   });
 
   it('rejects when something cannot be found', async () => {
-    await render(FixtureComponent);
-    await expect(screen.findByText('I am invisible', {}, { timeout: 40 })).rejects.toThrow('x');
+    const { findByText } = await render(FixtureComponent);
+    /// We whish to test the utility function from `render` here.
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    await expect(findByText('I am invisible', {}, { timeout: 40 })).rejects.toThrow('x');
   });
 });
