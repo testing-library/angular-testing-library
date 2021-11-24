cd17373272d517057c4650d2ccc13e61b07c27a6
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2021-11-24 21:43:11 +0100

    feat: add rerender method (#257)
    BREAKING CHANGE:
    
    `rerender` has been renamed to `change`.
    The `change` method keeps the current fixture intact and invokes `ngOnChanges`.
    
    The new `rerender` method destroys the current component and creates a new instance with the updated properties.
    
    BEFORE:
    
    ```ts
    const { rerender } = render(...)
    rerender({...})
    ```
    
    AFTER:
    
    ```ts
    const { change } = render(...)
    change({...})
    ```

--- apps/example-app-karma/src/app/issues/issue-222.spec.ts
+++ apps/example-app-karma/src/app/issues/issue-222.spec.ts
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/angular';
 
-it('https://github.com/testing-library/angular-testing-library/issues/222', async () => {
+it('https://github.com/testing-library/angular-testing-library/issues/222 with rerender', async () => {
   const { rerender } = await render(`<div>Hello {{ name}}</div>`, {
     componentProperties: {
       name: 'Sarah',
@@ -8,7 +8,21 @@ it('https://github.com/testing-library/angular-testing-library/issues/222', asyn
   });
 
   expect(screen.getByText('Hello Sarah')).toBeTruthy();
-  rerender({ name: 'Mark' });
+
+  await rerender({ name: 'Mark' });
+
+  expect(screen.getByText('Hello Mark')).toBeTruthy();
+});
+
+it('https://github.com/testing-library/angular-testing-library/issues/222 with change', async () => {
+  const { change } = await render(`<div>Hello {{ name}}</div>`, {
+    componentProperties: {
+      name: 'Sarah',
+    },
+  });
+
+  expect(screen.getByText('Hello Sarah')).toBeTruthy();
+  await change({ name: 'Mark' });
 
   expect(screen.getByText('Hello Mark')).toBeTruthy();
 });
