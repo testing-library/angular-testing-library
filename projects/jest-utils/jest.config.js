1e6a3f6fbd2c92ced582bbd6a6bf2c4660cee233
Rokas Brazdžionis <rokas.brazdzionis@gmail.com>
2022-06-09 13:47:02 +0300

    fix: use official secondary package format
    this fixes the `@testing-library/angular/spec-utils` import in Jest v28
    closes #299

--- projects/jest-utils/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'Jest utils',
-  displayName: {
-    name: 'JEST UTILS',
-    color: 'magenta',
-  },
-  preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
-};
