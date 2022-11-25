50aa0ac01910619892c5c499fe1b8c05d997da2d
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-11-25 13:23:23 +0100

    feat: add ability to turn off auto detect changes (#329)

--- projects/testing-library/tests/fire-event.spec.ts
+++ projects/testing-library/tests/fire-event.spec.ts
@@ -1,17 +1,48 @@
 import { Component } from '@angular/core';
 import { render, fireEvent, screen } from '../src/public_api';
+import { FormsModule } from '@angular/forms';
 
-@Component({
-  selector: 'atl-fixture',
-  template: ` <input type="text" data-testid="input" /> `,
-})
-class FixtureComponent {}
+describe('fireEvent', () => {
+  @Component({
+    selector: 'atl-fixture',
+    template: ` <input type="text" data-testid="input" [(ngModel)]="name" name="name" />
+      <div>Hello {{ name }}</div>`,
+  })
+  class FixtureComponent {
+    name = '';
+  }
 
-test('does not call detect changes when fixture is destroyed', async () => {
-  const { fixture } = await render(FixtureComponent);
+  it('automatically detect changes when event is fired', async () => {
+    await render(FixtureComponent, {
+      imports: [FormsModule],
+    });
 
-  fixture.destroy();
+    fireEvent.input(screen.getByTestId('input'), { target: { value: 'Tim' } });
 
-  // should otherwise throw
-  fireEvent.input(screen.getByTestId('input'), { target: { value: 'Bonjour' } });
+    expect(screen.getByText('Hello Tim')).toBeInTheDocument();
+  });
+
+  it('can disable automatic detect changes when event is fired', async () => {
+    const { detectChanges } = await render(FixtureComponent, {
+      imports: [FormsModule],
+      autoDetectChanges: false,
+    });
+
+    fireEvent.input(screen.getByTestId('input'), { target: { value: 'Tim' } });
+
+    expect(screen.queryByText('Hello Tim')).not.toBeInTheDocument();
+
+    detectChanges();
+
+    expect(screen.getByText('Hello Tim')).toBeInTheDocument();
+  });
+
+  it('does not call detect changes when fixture is destroyed', async () => {
+    const { fixture } = await render(FixtureComponent);
+
+    fixture.destroy();
+
+    // should otherwise throw
+    fireEvent.input(screen.getByTestId('input'), { target: { value: 'Bonjour' } });
+  });
 });
