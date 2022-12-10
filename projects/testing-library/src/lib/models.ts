cc465d7cf6dcac2defed4096c3249a59d05e8715
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-12-10 15:21:36 +0100

    feat: childComponentOverrides is stable

--- projects/testing-library/src/lib/models.ts
+++ projects/testing-library/src/lib/models.ts
@@ -255,7 +255,6 @@ export interface RenderComponentOptions<ComponentType, Q extends Queries = typeo
    *  ]
    * })
    *
-   * @experimental
    */
   childComponentOverrides?: ComponentOverride<any>[];
   /**
