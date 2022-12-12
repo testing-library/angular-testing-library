13c5bd59a7026f4f1b9c6e8064776417d87b02e6
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-12-12 19:00:00 +0100

    add test example for harness

---
+++ apps/example-app/src/app/examples/20-test-harness.spec.ts
@@ -0,0 +1,33 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatSnackBarHarness } from '@angular/material/snack-bar/testing';
+import { render, screen } from '@testing-library/angular';
+import user from '@testing-library/user-event';
+
+import { SnackBarComponent } from './20-test-harness';
+
+test('can be used with TestHarness', async () => {
+  const view = await render(`<app-harness></app-harness>`, {
+    imports: [SnackBarComponent],
+  });
+  const loader = TestbedHarnessEnvironment.documentRootLoader(view.fixture);
+
+  const buttonHarness = await loader.getHarness(MatButtonHarness);
+  const button = await buttonHarness.host();
+  button.click();
+
+  const snackbarHarness = await loader.getHarness(MatSnackBarHarness);
+  expect(await snackbarHarness.getMessage()).toMatch(/Pizza Party!!!/i);
+});
+
+test('can be used in combination with TestHarness', async () => {
+  const view = await render(SnackBarComponent);
+  const loader = TestbedHarnessEnvironment.documentRootLoader(view.fixture);
+
+  user.click(screen.getByRole('button'));
+
+  const snackbarHarness = await loader.getHarness(MatSnackBarHarness);
+  expect(await snackbarHarness.getMessage()).toMatch(/Pizza Party!!!/i);
+
+  expect(screen.getByText(/Pizza Party!!!/i)).toBeInTheDocument();
+});
