b1ac6b8c855140ff8043f17ab5417ea46559a00e
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2024-02-26 19:38:02 +0100

    docs: add signal input component test

--- apps/example-app/src/app/examples/22-signal-inputs.component.spec.ts
+++ apps/example-app/src/app/examples/22-signal-inputs.component.spec.ts
@@ -1,21 +1,33 @@
 import { render, screen } from '@testing-library/angular';
 import { SignalInputComponent } from './22-signal-inputs.component';
-import { Component } from '@angular/core';
 
-test('works with signal inputs using a wrapper component', async () => {
-  @Component({
-    template: `
-      <app-signal-input [greeting]="greeting" [name]="name"/>
-    `,
-    standalone: true,
-    imports: [SignalInputComponent],
-  })
-  class WrapperComponent {
-    greeting = 'Hello';
-    name = 'world';
-  }
-
-  await render(WrapperComponent);
+test('works with signal inputs', async () => {
+  await render(SignalInputComponent, {
+    componentInputs: {
+      name: 'world',
+      greeting: 'Hello',
+    },
+  });
 
   expect(screen.getByText(/hello world/i)).toBeInTheDocument();
+});
+
+test('works with signal inputs and rerenders', async () => {
+  const view = await render(SignalInputComponent, {
+    componentInputs: {
+      greeting: 'Hello',
+      name: 'world',
+    },
+  });
+
+  expect(screen.getByText(/hello world/i)).toBeInTheDocument();
+
+  await view.rerender({
+    componentInputs: {
+      greeting: 'bye',
+      name: 'test',
+    },
+  });
+
+  expect(screen.getByText(/bye test/i)).toBeInTheDocument();
 });
