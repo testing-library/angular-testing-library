ef10fbfed2738cef7b3e912557eb6f89d39c443b
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2024-06-21 20:11:36 +0200

    feat: add migration to add DTL as devDependency (#458)

--- projects/testing-library/test-setup.ts
+++ projects/testing-library/test-setup.ts
@@ -1,2 +1,6 @@
 import 'jest-preset-angular/setup-jest';
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+Object.assign(global, { TextDecoder, TextEncoder });
