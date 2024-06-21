4e3673e54acd7eb3201675571d3fdad98bd759f5
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2024-06-21 19:19:45 +0200

    feat: add DTL as devDependency on ng-add

--- projects/testing-library/schematics/ng-add/index.ts
+++ projects/testing-library/schematics/ng-add/index.ts
@@ -1,11 +1,27 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+  addPackageJsonDependency,
+  getPackageJsonDependency,
+  NodeDependencyType,
+} from '@schematics/angular/utility/dependencies';
+
+const dtl = '@testing-library/dom';
 
 export default function (): Rule {
-  return (host: Tree, context: SchematicContext) => {
+  return (tree: Tree, context: SchematicContext) => {
+    const dtlDep = getPackageJsonDependency(tree, dtl);
+    if (dtlDep) {
+      context.logger.info(`Skipping installation of '@testing-library/dom' because it's already installed.`);
+    } else {
+      context.logger.info(`Adding '@testing-library/dom' as a dev dependency.`);
+      addPackageJsonDependency(tree, { name: dtl, type: NodeDependencyType.Dev, overwrite: false, version: '^10.0.0' });
+    }
+
     context.logger.info(
       `Correctly installed @testing-library/angular.
 See our docs at https://testing-library.com/docs/angular-testing-library/intro/ to get started.`,
     );
-    return host;
+
+    return tree;
   };
 }
