33a0d00b33e227d41dd4f5762b4f03f1e84140c9
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2025-06-08 18:20:58 +0200

    fix tests

--- apps/example-app/src/test-setup.ts
+++ apps/example-app/src/test-setup.ts
@@ -1,9 +1,4 @@
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
-import { configure } from '@testing-library/angular';
 import '@testing-library/jest-dom';
 
 setupZoneTestEnv();
-configure({
-  defaultImports: [NoopAnimationsModule],
-});
