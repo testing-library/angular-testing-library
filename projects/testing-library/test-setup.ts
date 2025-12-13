55d4d252d2629f2dc0f9ec393aff2c72ebd860bf
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2025-12-13 19:03:30 +0100

    build: migrate to Angular workspace

--- projects/testing-library/test-setup.ts
+++ projects/testing-library/test-setup.ts
@@ -1,7 +1,4 @@
-import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
-
-setupZoneTestEnv();
 
 Object.assign(global, { TextDecoder, TextEncoder });
